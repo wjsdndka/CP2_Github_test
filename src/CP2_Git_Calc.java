a60ee7070b800e764bc5e019185540c61ba47ef2
깃허브 이름 <@.com>
2018-10-14 18:42:35 +0900

    .

---
+++ src/CP2_Git_Calc.java
@@ -0,0 +1,29 @@
+
+public class CP2_Git_Calc {
+
+	public static void main(String[] args) {
+		System.out.println(new Calc().add(1, 1));
+		System.out.println(new Calc().sub(6, 9));
+		System.out.println(new Calc().mul(5, 6));
+		System.out.println(new Calc().div(3, 5));
+	}
+
+}
+
+class Calc {
+	public int add(double a, double b) {
+		return -1;
+	}
+	
+	public int sub(double a, double b) {
+		return -1;
+	}
+	
+	public int mul(double a, double b) {
+		return -1;
+	}
+	
+	public int div(double a, double b) {
+		return -1;
+	}
+}
